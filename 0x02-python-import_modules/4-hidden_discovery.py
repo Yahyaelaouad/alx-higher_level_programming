3da57f6404594afda81e53be0461ccc3d5636a07
Yahyaelaouad <yahyaelaouad0@gmail.com>
2023-06-08 14:14:11 -0700

    Task 4

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,7 +1,11 @@
-#!/usr/bin/python3i
-# Owned By Yahya
+#!/usr/bin/python3
+#OWNED BY Yahya
+
 if __name__ == "__main__":
+    """Print all names defined by hidden_4 module."""
     import hidden_4
-    for i in dir(hidden_4):
-        if i.startswith("__") is False:
-            print(i)
+
+    names = dir(hidden_4)
+    for name in names:
+        if name[:2] != "__":
+            print(name)
