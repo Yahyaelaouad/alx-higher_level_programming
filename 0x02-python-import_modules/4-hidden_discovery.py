a509c777623fe777e5bd14f07d6190707d99e024
Yahyaelaouad <yahyaelaouad0@gmail.com>
2023-06-08 13:58:41 -0700

    Task 4

---
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+# Owned By Yahya
+
+import hidden_4
+def principal():
+    for i in dir(hidden_4):
+        if not (i[0] == '_' and i[1] == '_'):
+            print(i)
+
+if __name__ == "__main__":
+    principal()
