b739d8759b759856f69f1e2cc5a59a8e37397442
Yahyaelaouad <yahyaelaouad0@gmail.com>
2023-07-11 15:35:18 -0700

    Task 7

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,37 +1,24 @@
 #!/usr/bin/python3
-"""Add all arguments to a Python list and save them to a file."""
+"""
+Adds all arguments to a Python list and saves them to a JSON file
+"""
+
 import sys
-import json
 import os.path
+from save_to_json_file import save_to_json_file
+from load_from_json_file import load_from_json_file
 
+filename = "add_item.json"
 
-def save_to_json_file(my_obj, filename):
-    """Save an object to a JSON file."""
-    with open(filename, 'w') as file:
-        json.dump(my_obj, file)
+# Check if the file exists, load its content if it does
+if os.path.isfile(filename):
+    my_list = load_from_json_file(filename)
+else:
+    my_list = []
 
+# Add arguments to the list
+for arg in sys.argv[1:]:
+    my_list.append(arg)
 
-def load_from_json_file(filename):
-    """Load an object from a JSON file."""
-    with open(filename, 'r') as file:
-        return json.load(file)
-
-
-if __name__ == "__main__":
-    filename = "add_item.json"
-
-    # Check if the file exists, load its content if it does
-    if os.path.isfile(filename):
-        with open(filename, 'r') as file:
-            try:
-                items = json.load(file)
-            except json.JSONDecodeError:
-                items = []
-    else:
-        items = []
-
-    items.extend(sys.argv[1:])
-
-    # Save the updated list to the file
-    with open(filename, 'w') as file:
-        json.dump(items, file)
+# Save the updated list to the file
+save_to_json_file(my_list, filename)
